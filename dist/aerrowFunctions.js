95fee29a69b11b475b0a9783fd6743de0da0844d
PratikPatil <pratikpatil57432@gmail.com>
2025-05-26 13:27:36 +0530

    commits made

---
+++ dist/aerrowFunctions.js
@@ -0,0 +1,8 @@
+var multiply = function (num1, num2) {
+    return num1 * num2;
+};
+var out = multiply(2, 5);
+console.log(out);
+// one line version
+var square = function (num) { return (num * num); };
+console.log(square(5));
---
+++ src/aerrowFunctions.ts
@@ -0,0 +1,11 @@
+const multiply = (num1: number, num2: number): number => {
+    return num1 * num2;
+}
+let out = multiply(2, 5);
+console.log(out);
+
+
+// one line version
+
+const square = (num: number): number => (num * num);
+console.log(square(5));
