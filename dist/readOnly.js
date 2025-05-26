0817e9d81b46e705f695e39c7c2e17970268cec8
PratikPatil <pratikpatil57432@gmail.com>
2025-05-26 15:46:25 +0530

    ...

--- dist/readOnly.js
+++ dist/readOnly.js
@@ -1,16 +1,8 @@
-interface Electronics{
-    name: string;
-    readonly id: number;
-    purpose: string;
-}
-
-let mobile: Electronics={
+var mobile = {
     name: "Iphone",
     id: 4738,
     purpose: "entairtenment"
-}
-
+};
 mobile.name = "asus";
 // mobile.id = 12643; not possible only for reading purpose
-
 console.log(mobile);
