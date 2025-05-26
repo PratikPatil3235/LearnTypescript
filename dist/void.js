fc3c3a3fc1d2dd4fa8691d44dc65ddff53458294
PratikPatil <pratikpatil57432@gmail.com>
2025-05-26 11:26:46 +0530

    commits are made

---
+++ dist/void.js
@@ -0,0 +1,6 @@
+var userNameValue = "Pratik";
+var c = undefined;
+function displayDetails(name) {
+    console.log("User name is ".concat(name));
+}
+displayDetails(userNameValue);
---
+++ src/void.ts
@@ -0,0 +1,9 @@
+let userNameValue: string = "Pratik";
+
+let c: void = undefined;
+
+function displayDetails(name:string): void{
+    console.log(`User name is ${name}`);
+}
+
+displayDetails(userNameValue);
