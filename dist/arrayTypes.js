712377aa4820554d090aefc24c66bd221ffd30d6
PratikPatil <pratikpatil57432@gmail.com>
2025-05-26 16:03:46 +0530

    commits made

---
+++ dist/arrayTypes.js
@@ -0,0 +1,4 @@
+var idArray = [10, 1, 11, 13, 45, 4];
+var namesArray = ["Shyam", "Ram", "Karan", "Arjun"];
+//namesArray.push(4); not allowed
+console.log(idArray, namesArray);
---
+++ src/arrayTypes.ts
@@ -0,0 +1,5 @@
+let idArray: number[] = [10, 1, 11, 13, 45, 4];
+let namesArray: Array<string> = ["Shyam", "Ram", "Karan", "Arjun"];
+
+//namesArray.push(4); not allowed
+console.log(idArray, namesArray);
