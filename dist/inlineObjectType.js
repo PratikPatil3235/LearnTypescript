1925477118fec369df264b0ab474d1e05a56114e
PratikPatil <pratikpatil57432@gmail.com>
2025-05-26 14:26:36 +0530

    commits mad

---
+++ dist/inlineObjectType.js
@@ -0,0 +1,11 @@
+var user = {
+    name: "Patik",
+    age: 24,
+    location: {
+        city: "sangli",
+        pin: 416312
+    }
+};
+console.log(user.name);
+console.log(user.age);
+console.log(user);
---
+++ src/inlineObjectType.ts
@@ -0,0 +1,11 @@
+const user: { name: string; age: number , location:{ city: string; pin: number } } = {
+    name: "Patik",
+    age: 24,
+    location:{
+        city: "sangli",
+        pin:416312
+    }
+}
+console.log(user.name);
+console.log(user.age);
+console.log(user);
