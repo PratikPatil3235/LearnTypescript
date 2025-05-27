5d54d0c5d9a06a0fc78fe04d0bc459870a9f6be2
PratikPatil3235 <pratikpatil@medianv.com>
2025-05-27 15:53:39 +0530

    commits made

---
+++ dist/asKeyword.js
@@ -0,0 +1,4 @@
+// let values = "hello" as string;
+var values = "hello world";
+var len = values.length;
+console.log(len);
---
+++ src/asKeyword.ts
@@ -0,0 +1,6 @@
+// let values = "hello" as string;
+
+let values: unknown = "hello world";
+let len = (values as string).length;
+
+console.log(len);
