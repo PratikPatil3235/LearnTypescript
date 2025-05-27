1c6b07ec790cd735d0742ffb0e7159acef46bcd8
PratikPatil3235 <pratikpatil@medianv.com>
2025-05-27 15:21:16 +0530

    commits made

---
+++ dist/defaultGenericType.js
@@ -0,0 +1,8 @@
+var defaultType = {
+    data: "word",
+};
+console.log(defaultType.data);
+var customeType = {
+    data: 10
+};
+console.log(customeType.data);
---
+++ src/defaultGenericType.ts
@@ -0,0 +1,15 @@
+type ResponceType<T = string> = {
+    data: T;
+}
+
+const defaultType: ResponceType = {
+    data: "word",
+}
+
+console.log(defaultType.data);
+
+const customeType: ResponceType<number> = {
+    data:10
+}
+
+console.log(customeType.data);
