c0e695e9d015d7bf9362f75bfa2d1f4edace0189
PratikPatil3235 <pratikpatil@medianv.com>
2025-05-27 16:56:50 +0530

    commits made

---
+++ dist/templateLitralsType.js
@@ -0,0 +1,4 @@
+var filename = "message_en.json";
+var secondFilename = "message_fr.json";
+console.log(filename);
+console.log(secondFilename);
---
+++ src/templateLitralsType.ts
@@ -0,0 +1,7 @@
+type Lang = "en" | "fr";
+type FileName = `message_${Lang}.json`; 
+
+const filename: FileName = `message_en.json`;
+const secondFilename: FileName = `message_fr.json`;
+console.log(filename);
+console.log(secondFilename);
