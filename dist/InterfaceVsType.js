796aab382ae5cde2c02c07d5d1355d1bc6efd9c1
PratikPatil3235 <pratikpatil@medianv.com>
2025-05-27 14:25:38 +0530

    commits made

---
+++ dist/InterfaceVsType.js
@@ -0,0 +1,11 @@
+//Premetive typing allowed
+var myName = "Pratik";
+console.log(myName);
+var maskot = {
+    name: "sheru",
+    gender: "male",
+    sound: "bark"
+};
+console.log(maskot);
+var newName = "Pratik Patil";
+console.log(newName);
---
+++ src/InterfaceVsType.ts
@@ -0,0 +1,27 @@
+type PersonName = string;
+//Premetive typing allowed
+const myName: PersonName = "Pratik";
+console.log(myName);
+
+interface Animals{
+    name: string;
+    gender: string;
+}
+const maskot:Animals={
+    name: "sheru",
+    gender: "male",
+    sound:"bark"
+}
+console.log(maskot);
+
+interface Animals{
+    sound:string
+}
+
+type LastName = string;
+
+type NewName = PersonName & LastName
+
+const newName: NewName = "Pratik Patil"; 
+
+console.log(newName);
