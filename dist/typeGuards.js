9b28df177551ebc7e189f7ce6f6e4e8aef046868
PratikPatil3235 <pratikpatil@medianv.com>
2025-05-27 13:20:46 +0530

    commits made

---
+++ dist/typeGuards.js
@@ -0,0 +1,7 @@
+// typeof
+function checkTypeOf(str) {
+    if (typeof (str) == "string") {
+        console.log(str.length);
+    }
+}
+checkTypeOf("typescript");
---
+++ src/typeGuards.ts
@@ -0,0 +1,9 @@
+// typeof
+
+function checkTypeOf(str: string | number): void{
+    if (typeof (str) == "string") {
+        console.log(str.length);
+    }
+}
+
+checkTypeOf("typescript");
