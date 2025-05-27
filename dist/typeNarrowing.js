c06b28dfe47a9cffa4c36b57e0761e3a097e190c
PratikPatil3235 <pratikpatil@medianv.com>
2025-05-27 12:09:37 +0530

    commits made

---
+++ dist/typeNarrowing.js
@@ -0,0 +1,10 @@
+function accept(value) {
+    if (typeof (value) == "string") {
+        console.log(value.toUpperCase());
+    }
+    else {
+        console.log(value.toFixed(2));
+    }
+}
+accept("hello world");
+accept(3.1498534);
---
+++ src/typeNarrowing.ts
@@ -0,0 +1,12 @@
+function accept(value:string|number):void {
+    if (typeof (value) == "string") {
+        console.log(value.toUpperCase());
+    }
+    else {
+        console.log(value.toFixed(2));
+    }
+}
+
+accept("hello world");
+accept(3.1498534)
+
