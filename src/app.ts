dd476ced240a54fd22fb2807dcd34ff24ef148c6
PratikPatil3235 <pratikpatil@medianv.com>
2025-05-28 10:32:32 +0530

    commits made

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+
+import { greet, version } from "./some-lib";
+
+console.log(greet("Pratik"));
+console.log("Library version:", version);
---
+++ types/some-lib.d.ts
@@ -0,0 +1,4 @@
+declare module "some-lib" {
+  export function greet(name: string): string;
+  export const version: string;
+}
